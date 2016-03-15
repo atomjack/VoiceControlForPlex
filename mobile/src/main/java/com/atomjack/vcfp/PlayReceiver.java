e7cfbd70fb6d2b584146bf49f63ff61f1f60144b
Chris Bellew <atomjack@gmail.com>
2016-03-15 13:03:53 -0700

    Pass resume option when receiving voice input from Google Now.

--- mobile/src/main/java/com/atomjack/vcfp/PlayReceiver.java
+++ mobile/src/main/java/com/atomjack/vcfp/PlayReceiver.java
@@ -6,27 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.atomjack.shared.Logger;
+import com.atomjack.shared.Preferences;
 import com.atomjack.vcfp.services.PlexSearchService;
 
 public class PlayReceiver extends BroadcastReceiver
 {
-	@Override
-	public void onReceive(final Context context, final Intent intent)
-	{
-		Logger.d("PLAYRECEIVER");
-		Bundle bundle = intent.getExtras();
-		String queryText = bundle.getString(com.atomjack.shared.Intent.ARGUMENTS);
-		if(queryText == null && intent.getStringExtra(GoogleSearchApi.KEY_QUERY_TEXT) != null)
-			queryText = intent.getStringExtra(GoogleSearchApi.KEY_QUERY_TEXT);
+  @Override
+  public void onReceive(final Context context, final Intent intent)
+  {
+    Logger.d("PLAYRECEIVER");
+    Bundle bundle = intent.getExtras();
+    String queryText = bundle.getString(com.atomjack.shared.Intent.ARGUMENTS);
+    if(queryText == null && intent.getStringExtra(GoogleSearchApi.KEY_QUERY_TEXT) != null)
+      queryText = intent.getStringExtra(GoogleSearchApi.KEY_QUERY_TEXT);
 
-		if(queryText != null && queryText.matches(context.getResources().getString(R.string.pattern_recognition))) {
-			queryText = queryText.toLowerCase();
-			Intent sendIntent = new Intent(context, PlexSearchService.class);
-			sendIntent.putExtra(com.atomjack.shared.Intent.EXTRA_QUERYTEXT, queryText);
-			sendIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
-			sendIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-			sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			context.startService(sendIntent);
-		}
-	}
+    if(queryText != null && queryText.matches(context.getResources().getString(R.string.pattern_recognition))) {
+      queryText = queryText.toLowerCase();
+      Intent sendIntent = new Intent(context, PlexSearchService.class);
+      sendIntent.putExtra(com.atomjack.shared.Intent.EXTRA_QUERYTEXT, queryText);
+      sendIntent.putExtra(com.atomjack.shared.Intent.EXTRA_RESUME, VoiceControlForPlexApplication.getInstance().prefs.get(Preferences.RESUME, false));
+      sendIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+      sendIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+      sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      context.startService(sendIntent);
+    }
+  }
 }
